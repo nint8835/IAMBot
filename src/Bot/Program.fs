fd3f5416e7f92fab496be191cf4ad51f598643b9
Riley Flynn <riley@rileyflynn.me>
2023-06-23 23:10:44 -0230

    Basic bot connection This can almost certainly be done a million times better, but it works

--- src/Bot/Program.fs
+++ src/Bot/Program.fs
@@ -1,5 +1,12 @@
-﻿open dotenv.net
+﻿open System.Threading.Tasks
+open Discord
+open Discord.WebSocket
+open dotenv.net
 open Microsoft.Extensions.Configuration
+
+let Log (msg: LogMessage) : Task =
+    printfn $"%s{msg.ToString()}"
+    Task.CompletedTask
 
 type Configuration = { DiscordToken: string }
 
@@ -11,4 +18,20 @@ let config =
         .Build()
         .Get<Configuration>()
 
-printf $"{config.DiscordToken}"
+let socketConfig = DiscordSocketConfig()
+socketConfig.GatewayIntents <- GatewayIntents.AllUnprivileged ||| GatewayIntents.MessageContent
+
+let client = new DiscordSocketClient(socketConfig)
+client.add_Log Log
+
+client.add_MessageReceived (fun msg ->
+    printfn $"%s{msg.Content}"
+    Task.CompletedTask)
+
+client.LoginAsync(TokenType.Bot, config.DiscordToken)
+|> Async.AwaitTask
+|> Async.RunSynchronously
+
+client.StartAsync() |> Async.AwaitTask |> Async.RunSynchronously
+
+Task.Delay -1 |> Async.AwaitTask |> Async.RunSynchronously
