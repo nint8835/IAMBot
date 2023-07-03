1f5647ca71422719dd4d52980d15dd67c91d32cc
Riley Flynn <riley@rileyflynn.me>
2023-07-03 01:45:57 -0230

    Functional single executable

--- src/Bot/Program.fs
+++ src/Bot/Program.fs
@@ -11,41 +11,46 @@ type Configuration =
     { DiscordToken: string
       GuildId: uint64 }
 
-DotEnv.Load()
 
-let config =
-    ConfigurationBuilder()
-        .AddEnvironmentVariables("IAM_")
-        .Build()
-        .Get<Configuration>()
+[<EntryPoint>]
+let main (_: string[]) =
+    DotEnv.Load()
+
+    let config =
+        ConfigurationBuilder()
+            .AddEnvironmentVariables("IAM_")
+            .Build()
+            .Get<Configuration>()
 
 
-let reference = Reference.Load() |> Async.RunSynchronously
+    let reference = Reference.Load() |> Async.RunSynchronously
 
-let serviceProvider =
-    ServiceCollection()
-        .AddSingleton<Configuration>(config)
-        .AddSingleton<ReferenceFile>(reference)
-        .BuildServiceProvider()
+    let serviceProvider =
+        ServiceCollection()
+            .AddSingleton<Configuration>(config)
+            .AddSingleton<ReferenceFile>(reference)
+            .BuildServiceProvider()
 
-let client =
-    new DiscordClient(
-        DiscordConfiguration(
-            Token = config.DiscordToken,
-            TokenType = TokenType.Bot,
-            Intents = (DiscordIntents.AllUnprivileged ||| DiscordIntents.MessageContents)
+    let client =
+        new DiscordClient(
+            DiscordConfiguration(
+                Token = config.DiscordToken,
+                TokenType = TokenType.Bot,
+                Intents = (DiscordIntents.AllUnprivileged ||| DiscordIntents.MessageContents)
+            )
         )
-    )
 
-let slash =
-    client.UseSlashCommands(SlashCommandsConfiguration(Services = serviceProvider))
+    let slash =
+        client.UseSlashCommands(SlashCommandsConfiguration(Services = serviceProvider))
 
-slash.RegisterCommands<ActionCommands>(config.GuildId)
+    slash.RegisterCommands<ActionCommands>(config.GuildId)
 
-client.add_MessageCreated (fun _ evt -> task { printfn $"{evt.Message.Content}" })
+    client.add_MessageCreated (fun _ evt -> task { printfn $"{evt.Message.Content}" })
 
-task {
-    do! client.ConnectAsync()
-    do! Task.Delay -1
-}
-|> Task.WaitAll
+    task {
+        do! client.ConnectAsync()
+        do! Task.Delay -1
+    }
+    |> Task.WaitAll
+
+    0
